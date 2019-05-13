58dbcce04e0b181ffbd8dd07d961c98e91f7ee21
Calvin Wilkinson <kinsondigital@gmail.com>
2019-05-13 09:04:30 -0400

    Add project files.
    asdf

---
+++ NativeLibLoader/AssemblyLoader.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Runtime.Loader;
+using System.Text;
+
+namespace NativeLibLoader
+{
+    public class AssemblyLoader : AssemblyLoadContext
+    {
+        protected override Assembly Load(AssemblyName assemblyName)
+        {
+            if (assemblyName.Name == "")
+                return LoadFromAssemblyPath("");
+
+
+            return null;
+        }
+
+
+        protected override IntPtr LoadUnmanagedDll(string unmanagedDllName)
+        {
+            return base.LoadUnmanagedDll("");
+        }
+    }
+}
---
+++ NativeLibLoader/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace NativeLibLoader
+{
+    class Program
+    {
+#if WINDOWS
+        [DllImport("user32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        public static extern int MessageBox(IntPtr hWnd, string lpText, string lpCaption, uint uType);
+#elif LINUX
+        [DllImport("libSystem.dylib")]
+        private static extern int getpid();
+#endif
+
+
+        static void Main(string[] args)
+        {
+#if WINDOWS
+            MessageBox(IntPtr.Zero, $"This is a windows application.", "Win Msg Box", 0);
+#elif LINUX
+            int pid = getpid();
+            Console.WriteLine($"Process ID: {pid}");
+#endif
+            Console.ReadLine();
+        }
+    }
+}
